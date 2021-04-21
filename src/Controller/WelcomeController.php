b8a2a0c9fa1dcbaff724c9539482246db7e60a29
Anya Kovalieva <annakovalieva@gmail.com>
2021-04-21 18:32:55 +0200

    initial commit

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/welcome")
+     */
+    public function homepage(): Response
+    {
+        return $this->render('welcome.html.twig', [
+            'day' => date('l')
+        ]); 
+    }
+}
