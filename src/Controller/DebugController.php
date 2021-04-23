ce11a11b8b6c2d6f51d642d23c4828df6bf44cc8
Anya Kovalieva <annakovalieva@gmail.com>
2021-04-23 12:30:38 +0200

    debug controller and template added

---
+++ src/Controller/DebugController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use function Mos\Functions\{
+    url,
+    getBaseUrl,
+    getCurrentUrl,
+    getRoutePath
+};
+
+// require(INSTALL_PATH . '/src/functions.php');
+
+class DebugController extends AbstractController
+{
+    /**
+     * @Route("/debug")
+     */
+    public function debug(): Response
+    {
+        $baseUrl = getBaseUrl();
+        $currentUrl = getCurrentUrl();
+        $routePath= getRoutePath();
+        // $server = $_SERVER;
+        return $this->render('debug.html.twig',
+            // body data
+            [   "header" => "Debug page",
+                "baseUrl" => $baseUrl,
+                "currentUrl" => $currentUrl,
+                "routePath" => $routePath,
+                // "server" => $_SERVER,
+            ]); 
+    }
+}
